01db7ede92e69c2dd852adb94908c5f4b24524e0
John Doe <johndoe@example.com>
2021-11-04 09:34:37 +0100

    init commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -20,16 +20,13 @@ import java.util.List;
 
 /**
  * @author Alexandre Debus
+ * @version 1
  */
 
 public class Main {
 
     public static void main(String[] args){
-
-
     remplir_liste_clients();
-
-
     remplir_liste_prospects();
 
     }
@@ -38,27 +35,34 @@ public class Main {
          */
         public static void remplir_liste_clients()
         {
+            //recupération de ma liste de clients
             List<Clients> ma_liste = List_clients.getMa_liste();
-            Clients cl = null;
+
+            //on crée des objets clients et on les ajoute à la liste
+
             try {
-                cl = new Clients("SARL_alex",
+                    Clients   cl = new Clients("SARL_alex",
                         "1", "rue Test", "54000", "NANCY",
-                        "monemail@a.com", "0606060606", "commentaires", 150000, 150);
-                Clients cla = new Clients("Sarl_test",
+                        "monemail@a.com", "0606060606", "commentaires",
+                     150000, 150);
+                    Clients cla = new Clients("Sarl_test",
                         "2", "rue Test", "54000", "NANCY",
-                        "monemail@a.com", "0606060606", "commentaires", 150001, 151);
-                Clients clb = new Clients("Sarl_alex",
+                        "monemail@a.com", "0606060606", "commentaires",
+                        150001, 151);
+                    Clients clb = new Clients("Sarl_alex",
                         "3", "rue Test", "54000", "NANCY",
-                        "monemail@a.com", "06060606060", "commentaires", 152000, 130);
-                ma_liste = new ArrayList<>();
-                ma_liste.add(cl);
-                ma_liste.add(cla);
-                ma_liste.add(clb);
-            } catch (MonException e) {
-                e.printStackTrace();
-            }
+                        "monemail@a.com", "06060606060", "commentaires",
+                        152000, 130);
+                    ma_liste = new ArrayList<>();
+                    ma_liste.add(cl);
+                    ma_liste.add(cla);
+                    ma_liste.add(clb);
+                } catch (MonException e)
+                    {
+                    e.printStackTrace();
+                    }
 
-
+            //affichage de la liste
             System.out.println(ma_liste);
 
 
@@ -69,29 +73,38 @@ public class Main {
          */
         public static void remplir_liste_prospects()
         {
+            //recupération de la liste  de prospects
             List<Prospects> ma_liste_prospects = List_prospects.getMaliste_prospects();
-            // Prospects pr = new Prospects("monprospect_test","1","rue test","54000","Nancy","mon_email@a.com","commentaires",,1);
-            //System.out.println(pr.toString());
+
             ma_liste_prospects = new ArrayList<>();
+            //ajout et création d'objet Prospects à la liste
             try {
                 DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
-                Prospects pr = new Prospects("Mon_prospect", "1", "Rue ma rue", "54000",
+                Prospects pr = new Prospects("Mon_prospect", "1", "Rue ma rue",
+                        "54000",
                         "Nancy"
-                        , "email@email.com", "0606060606", "commentaire", LocalDate.parse("19/02/2021", format), "oui");
-                Prospects pra = new Prospects("Mon_deuxieme_prospect", "2", "Rue ma rue", "54000",
+                        , "email@email.com", "0606060606", "commentaire",
+                        LocalDate.parse("19/02/2021", format), "oui");
+                Prospects pra = new Prospects("Mon_deuxieme_prospect", "2", "Rue ma rue",
+                        "54000",
                         "Nancy"
-                        , "email@email.com", "06060606", "commentaire", LocalDate.parse("20/02/2021", format), "oui");
-                Prospects prb = new Prospects("Mon_troisieme", "4", "Rue ma rue", "54000",
+                        , "email@email.com", "06060606",
+                        "commentaire", LocalDate.parse("20/02/2021", format), "oui");
+                Prospects prb = new Prospects("Mon_troisieme",
+                        "4", "Rue ma rue", "54000",
                         "Nancy"
-                        , "email@email.com", "060606060", "commentaire", LocalDate.parse("18/02/2021", format), "oui");
+                        , "email@email.com", "060606060", "commentaire",
+                        LocalDate.parse("18/02/2021", format), "oui");
                 ma_liste_prospects.add(pr);
                 ma_liste_prospects.add(pra);
                 ma_liste_prospects.add(prb);
 
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
+                } catch (ParseException e)
+                    {
+                        e.printStackTrace();
+                    }
+            //affichage de la liste
             System.out.println(ma_liste_prospects);
 
         }
