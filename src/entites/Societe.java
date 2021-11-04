e60659fc680bba759d9d8c81375a00ba5d888f3f
John Doe <johndoe@example.com>
2021-11-04 09:50:34 +0100

    second commit

--- src/entites/Societe.java
+++ src/entites/Societe.java
@@ -5,6 +5,7 @@ import Exceptions.MonException;
 /**
  * @author Alexandre Debus
  * classe Societe
+ * @version 1
  */
 public abstract class Societe {
     //attributs d'instance
@@ -19,7 +20,8 @@ public abstract class Societe {
     private  String Comments;
     //constructeur implicit
     public  Societe(String raison_sociale,String Numero_rue,String nom_marue,String code,String ville,String
-                    email,String telephone,String commentaires) {
+                    email,String telephone,String commentaires)
+    {
                 setId(id);
                 setRaison_sociale(raison_sociale);
                 setNumero_rue(Numero_rue);
@@ -47,13 +49,16 @@ public abstract class Societe {
     }
 
     public void setRaison_sociale(String raison_sociale)  {
-        if (raison_sociale.equals("")){
-
+        //filtre pour savoir si la raison sociale est nulle
+        if (raison_sociale.equals(""))
+        {
+            //on renvoie une erreur avec le message approprié
             try {
                 throw new  MonException("");
-            } catch (MonException e) {
-                System.out.println("la raison sociale ne peut pas etre null");
-            }
+                } catch (MonException e)
+                    {
+                        System.out.println("la raison sociale ne peut pas etre null");
+                    }
 
 
         }
@@ -64,23 +69,29 @@ public abstract class Societe {
         return numero_rue;
     }
 
-    public void setNumero_rue(String numero_rue) {
+    public void setNumero_rue(String numero_rue)
+    {
+        //on vérifie si le numéro de rue n'a pas été saisie
 
-        if (numero_rue.equals("")){
+        if (numero_rue.equals(""))
+        {
             try {
                 throw new MonException("");
-            } catch (MonException e) {
-                System.out.println("il manque un élément à votre adresse, il faut remplir numéro rue ");
-            }
+                } catch (MonException e)
+                    {
+                        System.out.println("il manque un élément à votre adresse, il faut remplir numéro rue ");
+                    }
         }
         this.numero_rue = numero_rue;
     }
 
-    public String getNom_rue() {
+    public String getNom_rue()
+    {
         return nom_rue;
     }
 
     public void setNom_rue(String nom_rue) {
+        //on vérifie si le nom de rue n'a pas été saisi
         if (nom_rue.equals("")){
             try {
                 throw new  MonException("");
@@ -91,17 +102,22 @@ public abstract class Societe {
         this.nom_rue = nom_rue;
     }
 
-    public String getCode_postal() {
+    public String getCode_postal()
+    {
         return Code_postal;
     }
 
-    public void setCode_postal(String code_postal) {
-        if (code_postal.equals("")){
+    public void setCode_postal(String code_postal)
+    {
+        //on verifie si le code postal est vide
+        if (code_postal.equals(""))
+        {
             try {
                 throw new MonException("");
-            } catch (MonException e) {
-                System.out.println("il manque un élément à votre adresse , il faut remplir le code postal ");
-            }
+                } catch (MonException e)
+                    {
+                        System.out.println("il manque un élément à votre adresse , il faut remplir le code postal ");
+                    }
         }
         Code_postal = code_postal;
     }
@@ -110,59 +126,80 @@ public abstract class Societe {
         return Ville;
     }
 
-    public void setVille(String ville) {
-        if (ville.equals("")){
+    public void setVille(String ville)
+    {
+        //on vérifie si la ville a bien été saisi
+        if (ville.equals(""))
+        {
             try {
                 throw new MonException("");
-            } catch (MonException e) {
-                System.out.println(" il manque un élément à votre adresse, il faut renseigner une ville");
-            }
+                } catch (MonException e)
+                    {
+                        System.out.println(" il manque un élément à votre adresse, il faut renseigner une ville");
+                    }
         }
         Ville = ville;
     }
 
-    public String getEmail() {
+    public String getEmail()
+    {
         return email;
     }
 
-    public void setEmail(String email) {
-        if (!email.contains("@")){
+    public void setEmail(String email)
+    {
+        //on vérifie si l'email ne contient pas  d' @
+        if (!email.contains("@"))
+        {
             try {
                 throw  new MonException("");
-            } catch (MonException e) {
-                System.out.println("ceci n'est pas une adresse mail, il ne contient pas d' @");
-            }
+                } catch (MonException e)
+                    {
+                        System.out.println("ceci n'est pas une adresse mail, il ne contient pas d' @");
+                    }
         }
         this.email = email;
     }
 
-    public String getNum_tel() {
+    public String getNum_tel()
+    {
         return num_tel;
     }
 
-    public void setNum_tel(String num_tel) {
-        if (num_tel.equals("")){
+    public void setNum_tel(String num_tel)
+    {
+        //on vérifie si le numéro de telephone a bien été saisie
+        if (num_tel.equals(""))
+        {
+            //on renvoie l' erreur
             try {
                 throw new MonException("");
             } catch (MonException e) {
                 System.out.println(" le numéro de teléphone ne peut pas etre vide veuillez le renseigner");
             }
-         if (num_tel.length()<10){
+        }
+        //on vérifie  que le numéro de telephone ne soit pas inférieur à 10 caractères
+         if (num_tel.length()<10)
+         {
+             //on renvoie une erreur avec le bon message
              try {
                  throw  new MonException("");
-             } catch (MonException e) {
-                 System.out.println("le numéro de téléphone doit avoir au moins 10 caractères");
-             }
+                  } catch (MonException e)
+                    {
+                        System.out.println("le numéro de téléphone doit avoir au moins 10 caractères");
+                    }
          }
-        }
+
         this.num_tel = num_tel;
     }
 
-    public String getComments() {
+    public String getComments()
+    {
         return Comments;
     }
 
-    public void setComments(String comments) {
+    public void setComments(String comments)
+    {
         Comments = comments;
     }
 
@@ -171,7 +208,8 @@ public abstract class Societe {
      * @return  chaine de caractères  des données de société
      */
     @Override
-    public String toString() {
+    public String toString()
+    {
         return Raison_sociale+" "+numero_rue+" "+nom_rue+"\n"+Code_postal+
                 " "+Ville+"  "+ email +" "+ Comments;
     }
